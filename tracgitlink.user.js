6d0c9b740e843700813978b188712f51234389b9
Max Lavrenov <max.lavrenov@gmail.com>
2011-10-18 15:01:43 +0400

    скрипт подменяющий урлы на git коммиты

---
+++ tracgitlink.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name          Autoreplace git commit hash with url
+// @namespace     http://netstream.ru/
+// @description   Autoreplace git commit hash with url
+// @include       http://dev-trac.netstream.ru/*
+// ==/UserScript==
+
+alert('test');
+res = document.getElementsByTagName('a');
+alert(res.length);
+var len = res.length;
+for(var i=0; i<len; i++) {
+	if (res[i].classList[0] == 'missing' & res[i].classList[1] == 'changeset') {
+		res[i].href = 'http://dev-trac.netstream.ru/changeset/' + res[i].innerHTML + '/git_da';
+	}
+
+}
